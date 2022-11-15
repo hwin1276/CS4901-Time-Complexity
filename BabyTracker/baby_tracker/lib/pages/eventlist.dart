33ec623b541b0f4fbf70795341dced992f0d3aa4
hpham474 <89870604+hpham474@users.noreply.github.com>
2022-11-15 07:44:10 -0600

    Made temporary button to add a test user in event tab

--- BabyTracker/baby_tracker/lib/pages/eventlist.dart
+++ BabyTracker/baby_tracker/lib/pages/eventlist.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../addadminuser.dart';
+
 class EventList extends StatefulWidget {
   const EventList({Key? key}) : super(key: key);
 
@@ -11,11 +13,14 @@ class _EventListState extends State<EventList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Text(
-                'Event View to be Added'
-            )
-        )
-    );
+        body: Container(
+            height: 40,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: AddAdminUser())
+        );
   }
 }
