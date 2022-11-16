b59d0a3a5a031f8391a8269a1c8a844585f27048
hpham474 <89870604+hpham474@users.noreply.github.com>
2022-11-15 18:34:02 -0600

    Added form validation to create event page

--- BabyTracker/baby_tracker/lib/createevent.dart
+++ BabyTracker/baby_tracker/lib/createevent.dart
@@ -11,7 +11,12 @@ class CreateEvent extends StatefulWidget {
 class _CreateEventState extends State<CreateEvent> {
   DateTime startDateTime = DateTime.now();
   DateTime endDateTime = DateTime.now();
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   String? dropdownValue;
+  int? eventtype;
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,208 +28,257 @@ class _CreateEventState extends State<CreateEvent> {
     final endMinutes = endDateTime.minute.toString().padLeft(2,'0');
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Add an Event'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal:10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal:15),
-                child: DropdownButtonFormField(
-                  value: dropdownValue,
-                  icon: Icon(Icons.arrow_downward),
-                  hint: Text('What kind of event are you adding?'),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      dropdownValue = newValue!;
-                    });
-                  },
-                  items: <String>[
-                    'Diaper Change',
-                    'Meal Time',
-                    'Sleep Time',
-                    'Accidents'
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ),
-              Divider(
-                color: Colors.grey,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal:15),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Title",
-                  ),
-                ),
-              ),
-              Divider(
-                color: Colors.grey,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal:15),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Description"
-                  ),
-                  maxLines: 5,
-                ),
-              ),
-              Divider(
-                color: Colors.grey,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal:15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Start Time:'),
-                    Row(
-                      children: [
-                        ElevatedButton(
-                          child: Text(
-                            '${startDateTime.year}/${startDateTime.month}/${startDateTime.day}',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                          onPressed: () async {
-                            final date = await pickDate(startDateTime);
+      appBar: AppBar(
+        title: const Text('Add an Event'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal:10, vertical: 15),
+          child: Form(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal:15),
+                  child: DropdownButtonFormField(
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    hint: Text('What kind of task are you adding?'),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
 
-                            if (date == null) return;
-
-                            final newDateTime = DateTime(
-                              date.year,
-                              date.month,
-                              date.day,
-                              startDateTime.hour,
-                              startDateTime.minute,
-                            );
-
-                            setState(() => startDateTime = newDateTime);
-
-                          },
-                        ),
-                        SizedBox(width: 10),
-                        ElevatedButton(
-                          child: Text(
-                            '$hours:$minutes',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                          onPressed: () async {
-                            final time = await pickTime(startDateTime.hour, startDateTime.minute);
-
-                            if (time == null) return;
-
-                            final newDateTime = DateTime(
-                              startDateTime.year,
-                              startDateTime.month,
-                              startDateTime.day,
-                              time.hour,
-                              time.minute,
-                            );
-
-                            setState(() => startDateTime = newDateTime);
-
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal:15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('End Time:'),
-                    Row(
-                      children: [
-                        ElevatedButton(
-                          child: Text(
-                            '${endDateTime.year}/${endDateTime.month}/${endDateTime.day}',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                          onPressed: () async {
-                            final date = await pickDate(endDateTime);
-
-                            if (date == null) return;
-
-                            final newDateTime = DateTime(
-                              date.year,
-                              date.month,
-                              date.day,
-                              endDateTime.hour,
-                              endDateTime.minute,
-                            );
-
-                            setState(() => endDateTime = newDateTime);
-
-                          },
-                        ),
-                        SizedBox(width: 10),
-                        ElevatedButton(
-                          child: Text(
-                            '$endHours:$endMinutes',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                          onPressed: () async {
-                            final time = await pickTime(endDateTime.hour, endDateTime.minute);
-
-                            if (time == null) return;
-
-                            final newDateTime = DateTime(
-                              endDateTime.year,
-                              endDateTime.month,
-                              endDateTime.day,
-                              time.hour,
-                              time.minute,
-                            );
-
-                            setState(() => endDateTime = newDateTime);
-
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Divider(
-                color: Colors.grey,
-              ),
-              SizedBox(height: 100),
-              Container(
-                  height: 40,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
+                        switch(dropdownValue) {
+                          case 'Diaper Change': {
+                            eventtype = 1;
+                          }
+                          break;
+                          case 'Meal Time': {
+                            eventtype = 2;
+                          }
+                          break;
+                          case 'Sleep Time': {
+                            eventtype = 3;
+                          }
+                          break;
+                          case 'Accidents': {
+                            eventtype = 4;
+                          }
+                          break;
+                        }
+                      });
                     },
-                    child: const Text(
-                        'Confirm',
-                        style: TextStyle(
-                          color: Colors.white,
-                        )
+                    items: <String>[
+                      'Diaper Change',
+                      'Meal Time',
+                      'Sleep Time',
+                      'Accidents'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please enter what kind of task your adding';
+                      }
+                      else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal:15),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "What is your task?",
                     ),
-                  )
-              )
-            ],
+                    controller: titleController,
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return 'Please enter your task';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal:15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Description"
+                    ),
+                    maxLines: 5,
+                    controller: descriptionController,
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal:15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Start Time:'),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            child: Text(
+                              '${startDateTime.year}/${startDateTime.month}/${startDateTime.day}',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            onPressed: () async {
+                              final date = await pickDate(startDateTime);
+
+                              if (date == null) return;
+
+                              final newDateTime = DateTime(
+                                date.year,
+                                date.month,
+                                date.day,
+                                startDateTime.hour,
+                                startDateTime.minute,
+                              );
+
+                              setState(() => startDateTime = newDateTime);
+
+                            },
+                          ),
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            child: Text(
+                              '$hours:$minutes',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            onPressed: () async {
+                              final time = await pickTime(startDateTime.hour, startDateTime.minute);
+
+                              if (time == null) return;
+
+                              final newDateTime = DateTime(
+                                startDateTime.year,
+                                startDateTime.month,
+                                startDateTime.day,
+                                time.hour,
+                                time.minute,
+                              );
+
+                              setState(() => startDateTime = newDateTime);
+
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal:15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('End Time:'),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            child: Text(
+                              '${endDateTime.year}/${endDateTime.month}/${endDateTime.day}',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            onPressed: () async {
+                              final date = await pickDate(endDateTime);
+
+                              if (date == null) return;
+
+                              final newDateTime = DateTime(
+                                date.year,
+                                date.month,
+                                date.day,
+                                endDateTime.hour,
+                                endDateTime.minute,
+                              );
+
+                              setState(() => endDateTime = newDateTime);
+
+                            },
+                          ),
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            child: Text(
+                              '$endHours:$endMinutes',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            onPressed: () async {
+                              final time = await pickTime(endDateTime.hour, endDateTime.minute);
+
+                              if (time == null) return;
+
+                              final newDateTime = DateTime(
+                                endDateTime.year,
+                                endDateTime.month,
+                                endDateTime.day,
+                                time.hour,
+                                time.minute,
+                              );
+
+                              setState(() => endDateTime = newDateTime);
+
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey,
+                ),
+                SizedBox(height: 100),
+                Container(
+                    height: 40,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        final isValidForm = formKey.currentState!.validate();
+                        if (isValidForm) {
+                          final task = titleController.text;
+                          final description = descriptionController.text;
+                          //add event to database
+                        }
+                      },
+                      child: const Text(
+                          'Confirm',
+                          style: TextStyle(
+                            color: Colors.white,
+                          )
+                      ),
+                    )
+                )
+              ],
+            ),
           ),
         ),
+      ),
     );
   }
+
   Future<DateTime?> pickDate(dateTime) => showDatePicker (
     context: context,
     initialDate:dateTime,
