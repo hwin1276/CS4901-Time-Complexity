99067b6c586155f82bc7dc95c2ad87a630918061
hnguyen <hnguyen1276@gmail.com>
2022-11-17 01:08:17 -0600

    Added gender
    Baby card color changes with respect to gender

--- BabyTracker/baby_tracker/lib/baby_card.dart
+++ BabyTracker/baby_tracker/lib/baby_card.dart
@@ -15,18 +15,38 @@ class BabyCard extends StatelessWidget {
       child: Container(
           margin: const EdgeInsets.all(20),
           height: 150,
-          decoration: const BoxDecoration(color: Colors.grey),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 190, 190, 190),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
           child: Stack(
             children: [
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Row(children: [
                   ClipOval(
-                    child: Container(
-                        color: Colors.blue[100],
-                        padding: const EdgeInsets.all(10),
-                        child: const Icon(Icons.child_care, size: 30)),
-                  ),
+                      child: baby.gender
+                          ? Container(
+                              // if male then color = blue
+                              color: Colors.blue[100],
+                              padding: const EdgeInsets.all(10),
+                              child: const Icon(Icons.child_care, size: 30))
+                          : Container(
+                              color: Colors.pink[100], // if female
+                              padding: const EdgeInsets.all(10),
+                              child: const Icon(Icons.child_care, size: 30))),
                   const SizedBox(width: 10),
                   Text(baby.name),
                 ]),
--- BabyTracker/baby_tracker/lib/objects/baby.dart
+++ BabyTracker/baby_tracker/lib/objects/baby.dart
@@ -4,21 +4,23 @@ class Baby {
   late int babyid;
   late int parentid;
   late String name;
+  late bool gender; // true = male , false = female
   List<Event>? events;
 
   Baby(
       {required this.babyid,
       required this.parentid,
       required this.name,
+      required this.gender,
       this.events});
 
   static List<Baby> babyList() {
     return [
       // Sample data for baby list
-      Baby(babyid: 0, parentid: 0, name: 'Frank'),
-      Baby(babyid: 1, parentid: 0, name: 'John'),
-      Baby(babyid: 2, parentid: 1, name: 'Alex'),
-      Baby(babyid: 3, parentid: 2, name: 'Dustin'),
+      Baby(babyid: 0, parentid: 0, name: 'Frank', gender: true),
+      Baby(babyid: 1, parentid: 0, name: 'John', gender: true),
+      Baby(babyid: 2, parentid: 1, name: 'Maria', gender: false),
+      Baby(babyid: 3, parentid: 2, name: 'Dustin', gender: true),
     ];
   }
 }
