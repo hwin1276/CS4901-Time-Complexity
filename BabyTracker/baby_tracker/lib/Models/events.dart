8060413c858f502951b8dd93711d75748207b33b
KennedyM163 <kennedym163@gmail.com>
2022-12-08 07:16:16 -0600

    adding functionality for stats

--- BabyTracker/baby_tracker/lib/Models/events.dart
+++ BabyTracker/baby_tracker/lib/Models/events.dart
@@ -1,75 +1,52 @@
-final String eventTable = 'events';
 
-class EventFields {
-  static final List<String> values = [
-    // all fields
-    childId, type, startTime, inputTime, endTime, amountFood, diaperChange
-  ];
-  
-    static final String childId = 'childId';
-    static final String type = 'type';
-    static final String startTime = 'startTime';
-    static final String inputTime = 'inputTime';
-    static final String endTime = 'endTime';
-    static final String amountFood = 'foodCount';
-    static final String diaperChange = 'diaperChange';
-}
+import 'dart:html';
 
 class Event {
-    final int? childId;
-    final String type;
-    final DateTime startTime;
-    final DateTime inputTime;
-    final DateTime endTime;
-    final int amountFood;
-    final String diaperChange;
- 
-    const Event({
-        required this.childId,
-        required this.type,
-        required this.inputTime,
-        required this.startTime,
-        required this.endTime,
-        required this.amountFood,
-        required this.diaperChange,
-    });
+  final String type;
+  final String task;
+  final String description;
+  DateTime start;
+  DateTime end;
+  Event(this.type, this.task, this.description,this.start,this.end);
 
-Event copy({
-    int? childId,
-    String? type,
-    DateTime? startTime,
-    DateTime? inputTime,
-    DateTime? endTime,
-    int? amountfood,
-    String? diaperChange,
-  }) =>
-      Event(
-        childId: childId ?? this.childId,
-        type: type ?? this.type,
-        startTime: startTime ?? this.startTime,
-        inputTime: inputTime ?? this.inputTime,
-        endTime: endTime ?? this.endTime,
-        amountFood: amountFood ?? this.amountFood,
-        diaperChange: diaperChange ?? this.diaperChange,
-      );
+  Event.fromMap(Map<String, dynamic> map)
+      : assert(map['type'] != null),
+      assert(map['task'] != null),
+      assert(map['description'] != null),
+      assert(map['start'] != null),
+      assert(map['end'] != null),
+      type = map['type'],
+      task = map['task'],
+      description = map['description'],
+      start = map['start'],
+      end = map['end'];
 
-static Event fromJson(Map<String, Object?> json) => Event(
-        childId: json[EventFields.childId] as int?,
-        type: json[EventFields.type] as String,
-        amountFood: json[EventFields.amountFood] as int,
-        startTime: DateTime.parse(json[EventFields.startTime] as String),
-        endTime: DateTime.parse(json[EventFields.endTime] as String),
-        inputTime: DateTime.parse(json[EventFields.inputTime] as String),
-        diaperChange: json[EventFields.diaperChange] as String,
-      );
-
-Map<String, Object?> toJson() => {
-        EventFields.childId: childId,
-        EventFields.type: type,
-        EventFields.amountFood: amountFood,
-        EventFields.startTime: startTime.toIso8601String(),
-        EventFields.endTime: endTime.toIso8601String(),
-        EventFields.inputTime: inputTime.toIso8601String(),
-        EventFields.diaperChange: diaperChange,
-      };
 }
+// Event copy({
+//     int? childId,
+//     String? type,
+//     DateTime? startTime,
+//     DateTime? inputTime,
+//     DateTime? endTime,
+//     int? amountfood,
+//     String? diaperChange,
+//   }) =>
+//       Event(
+//         childId: childId ?? this.childId,
+//         type: type ?? this.type,
+//         startTime: startTime ?? this.startTime,
+//         inputTime: inputTime ?? this.inputTime,
+//         endTime: endTime ?? this.endTime,
+//         amountFood: amountFood ?? this.amountFood,
+//         diaperChange: diaperChange ?? this.diaperChange,
+//       );
+
+// static Event fromJson(Map<String, Object?> json) => Event(
+//         childId: json[EventFields.childId] as int?,
+//         type: json[EventFields.type] as String,
+//         amountFood: json[EventFields.amountFood] as int,
+//         startTime: DateTime.parse(json[EventFields.startTime] as String),
+//         endTime: DateTime.parse(json[EventFields.endTime] as String),
+//         inputTime: DateTime.parse(json[EventFields.inputTime] as String),
+//         diaperChange: json[EventFields.diaperChange] as String,
+//       );
--- BabyTracker/baby_tracker/lib/pages/statistics.dart
+++ BabyTracker/baby_tracker/lib/pages/statistics.dart
@@ -1,21 +1,98 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:baby_tracker/Models/events.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
 
 class Statistics extends StatefulWidget {
-  const Statistics({Key? key}) : super(key: key);
+  //const Statistics({Key? key}) : super(key: key);
 
   @override
   State<Statistics> createState() => _StatisticsState();
 }
 
 class _StatisticsState extends State<Statistics> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Text(
-                'Statistics View to be Added'
-            )
-        )
+
+  DateTime today = new DateTime.now(); // this is for getting all data from today
+  // the below is where I create a list for the bar graph
+  List<charts.Series<Event,String>> _barGraphData = [];
+  List<Event> babyData = [];
+  _generateData(babyData){
+   //_barGraphData = List<charts.Series<Event, String>>.empty();
+    _barGraphData.add(
+      charts.Series(
+        domainFn: (Event events,_)=> events.type.toString(), // x axis value 
+        measureFn: (Event events,_)=> babyData.addAll(events.start.toString().substring(0,10) == today.toString().substring(0,10)), //y axis calculating only inputs for today
+        //measureFn: (Event events,_)=> babyData.addAll(events), // y axis values
+        colorFn: (Event events,_)=> charts.MaterialPalette.blue.shadeDefault,
+        id: 'Daily-Events',
+        data: babyData,
+      )
     );
   }
+  
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todays Events')),
+        body: _buildBody(context),
+            // child: Text(
+            //     'Statistics View to be Added'
+            //)
+        );
+  }
+
+  Widget _buildBody(context){
+    return StreamBuilder<QuerySnapshot<Map<String,dynamic>>>(
+      stream: FirebaseFirestore.instance.collection('events').snapshots(),
+      builder: (context, snapshot) {
+        if(!snapshot.hasData){
+          return LinearProgressIndicator();
+        } else {
+        List<Event> events = snapshot.data!.docs.map((documentSnapshot)=> Event.fromMap(documentSnapshot.data())).toList();
+        return _buildChart(context, events);
+        }
+      },
+    );
+  }
+
+ Widget _buildChart(BuildContext context, List<Event> data) {
+    babyData = data;
+    _generateData(babyData);
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Events input today',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                child: charts.BarChart(_barGraphData,
+                    animate: true,
+                    animationDuration: Duration(seconds:2),
+                     behaviors: [
+                      charts.DatumLegend(
+                        entryTextStyle: charts.TextStyleSpec(
+                            color: charts.MaterialPalette.purple.shadeDefault,
+                            fontFamily: 'Georgia',
+                            fontSize: 18),
+                      )
+                    ],
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 }
