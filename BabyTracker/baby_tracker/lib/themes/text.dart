4eba749ae2336849e228288106b51d5b07d0c397
April Eaton <neaton12416@gmail.com>
2023-03-06 14:57:05 -0600

    Add h2 text style

--- BabyTracker/baby_tracker/lib/themes/text.dart
+++ BabyTracker/baby_tracker/lib/themes/text.dart
@@ -4,14 +4,21 @@ import 'package:flutter/material.dart';
 class AppTextTheme {
   static const body = TextStyle(
     fontSize: 16,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeight.w300,
     color: AppColorScheme
         .red, // This isn't really the color it should ever be, use .copyWith() method to set the proper color
   );
   static const h1 = TextStyle(
     fontSize: 30,
     fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeight.w800,
+    color: AppColorScheme
+        .red, // This isn't really the color it should ever be, use .copyWith() method to set the proper color
+  );
+  static const h2 = TextStyle(
+    fontSize: 26,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
     color: AppColorScheme
         .red, // This isn't really the color it should ever be, use .copyWith() method to set the proper color
   );
