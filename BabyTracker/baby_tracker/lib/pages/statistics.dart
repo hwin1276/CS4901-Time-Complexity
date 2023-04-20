567ed7164170ce151cf48db2d193eafaf9c544e3
Cecil <90416507+innodim1@users.noreply.github.com>
2023-04-20 12:59:36 -0500

    Update statistics.dart
    stats page

--- BabyTracker/baby_tracker/lib/pages/statistics.dart
+++ BabyTracker/baby_tracker/lib/pages/statistics.dart
@@ -1,93 +1,119 @@
-import 'package:baby_tracker/widgets/event_list.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:intl/intl.dart';
 
-
-class Statistics extends StatefulWidget {
-  @override
-  _StatisticsState createState() => _StatisticsState();
+void main() {
+  runApp(MyApp());
 }
 
-class _StatisticsState extends State<Statistics> {
-  List<Map<String, dynamic>> _data = [
-    {
-      'event': 'Diaper Change',
-      'data': [
-        {'time': DateTime.now().subtract(Duration(days: 6)), 'value': 4},
-        {'time': DateTime.now().subtract(Duration(days: 5)), 'value': 5},
-        {'time': DateTime.now().subtract(Duration(days: 4)), 'value': 3},
-        {'time': DateTime.now().subtract(Duration(days: 3)), 'value': 6},
-        {'time': DateTime.now().subtract(Duration(days: 2)), 'value': 2},
-        {'time': DateTime.now().subtract(Duration(days: 1)), 'value': 3},
-        {'time': DateTime.now(), 'value': 5},
-      ],
-    },
-    {
-      'event': 'Feeding',
-      'data': [
-        {'time': DateTime.now().subtract(Duration(days: 6)), 'value': 5},
-        {'time': DateTime.now().subtract(Duration(days: 5)), 'value': 6},
-        {'time': DateTime.now().subtract(Duration(days: 4)), 'value': 4},
-        {'time': DateTime.now().subtract(Duration(days: 3)), 'value': 7},
-        {'time': DateTime.now().subtract(Duration(days: 2)), 'value': 3},
-        {'time': DateTime.now().subtract(Duration(days: 1)), 'value': 4},
-        {'time': DateTime.now(), 'value': 6},
-      ],
-    },
-    {
-      'event': 'Sleeping',
-      'data': [
-        {'time': DateTime.now().subtract(Duration(days: 6)), 'value': 6},
-        {'time': DateTime.now().subtract(Duration(days: 5)), 'value': 7},
-        {'time': DateTime.now().subtract(Duration(days: 4)), 'value': 5},
-        {'time': DateTime.now().subtract(Duration(days: 3)), 'value': 8},
-        {'time': DateTime.now().subtract(Duration(days: 2)), 'value': 4},
-        {'time': DateTime.now().subtract(Duration(days: 1)), 'value': 5},
-        {'time': DateTime.now(), 'value': 7},
-      ],
-    },
-    {
-      'event': 'Meal Per Day',
-      'data': [
-        {'time': DateTime.now().subtract(Duration(days: 6)), 'value': 2},
-        {'time': DateTime.now().subtract(Duration(days: 5)), 'value': 3},
-        {'time': DateTime.now().subtract(Duration(days: 4)), 'value': 2},
-        {'time': DateTime.now().subtract(Duration(days: 3)), 'value': 4},
-        {'time': DateTime.now().subtract(Duration(days: 2)), 'value': 2},
-        {'time': DateTime.now().subtract(Duration(days: 1)), 'value': 3},
-        {'time': DateTime.now(), 'value': 4},
-      ],
-    },
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StatisticsPage(),
+    );
+  }
+}
+
+class StatisticsPage extends StatelessWidget {
+  final List<BabyStat> diaperChangesData = [
+    BabyStat(label: 'Mon', value: 6),
+    BabyStat(label: 'Tue', value: 8),
+    BabyStat(label: 'Wed', value: 7),
+    BabyStat(label: 'Thu', value: 5),
+    BabyStat(label: 'Fri', value: 6),
+    BabyStat(label: 'Sat', value: 4),
+    BabyStat(label: 'Sun', value: 9),
   ];
 
-  List<LineChartBarData> _lineChartData = [];
-  
-   @override
-  void initState() {
-    super.initState();
-    _generateData();
+  final List<BabyStat> feedingTimesData = [
+    BabyStat(label: 'Mon', value: 5),
+    BabyStat(label: 'Tue', value: 6),
+    BabyStat(label: 'Wed', value: 7),
+    BabyStat(label: 'Thu', value: 4),
+    BabyStat(label: 'Fri', value: 8),
+    BabyStat(label: 'Sat', value: 6),
+    BabyStat(label: 'Sun', value: 5),
+  ];
+
+  final List<BabyStat> sleepDurationsData = [
+    BabyStat(label: 'Mon', value: 12),
+    BabyStat(label: 'Tue', value: 14),
+    BabyStat(label: 'Wed', value: 11),
+    BabyStat(label: 'Thu', value: 13),
+    BabyStat(label: 'Fri', value: 15),
+    BabyStat(label: 'Sat', value: 10),
+    BabyStat(label: 'Sun', value: 14),
+  ];
+
+  StatisticsPage({Key? key}) : super(key: key);
+
+  Widget buildBarChart(String title, List<BabyStat> data) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: TextStyle(fontSize: 24)),
+        SizedBox(height: 20),
+        SizedBox(
+          height: 200,
+          child: BarChart(
+            BarChartData(
+              barTouchData: BarTouchData(enabled: false),
+              titlesData: FlTitlesData(
+                show: true,
+                bottomTitles: SideTitles(
+                  showTitles: true,
+                  getTextStyles: (value) => const TextStyle(color: Colors.black, fontSize: 12),
+                  getTitles: (double value) {
+                    return data[value.toInt()].label;
+                  },
+                ),
+                leftTitles: SideTitles(showTitles: false),
+              ),
+              borderData: FlBorderData(show: false),
+              barGroups: data.asMap().entries.map((entry) {
+                return BarChartGroupData(
+                  x: entry.key,
+                  barRods: [
+                    BarChartRodData(
+                      y: entry.value.value.toDouble(),
+                      colors: [Colors.blue],
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
-  void _generateData() {
-  for (var i = 0; i < _data.length; i++) {
-    _lineChartData.add(
-      LineChartBarData(
-        spots: _data[i]['data'].map((datum) {
-          final time = datum['time'] as DateTime;
-          final value = datum['value'] as int;
-          return FlSpot(time.millisecondsSinceEpoch.toDouble(), value.toDouble());
-        }).toList(),
-        isCurved: true,
-        colors: [Colors.blue],
-        barWidth: 2,
-        dotData: FlDotData(show: false),
-        belowBarData: BarAreaData(
-          show: true,
-          colors: [Colors.blue.withOpacity(0.3)],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Baby Tracker Statistics')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            buildBarChart('Diaper Changes per Day', diaperChangesData),
+            SizedBox(height: 40),
+            buildBarChart('Feeding Times per Day', feedingTimesData),
+            SizedBox(height: 40),
+                       buildBarChart('Sleep Durations per Day (hours)', sleepDurationsData),
+            // Add other statistics widgets here
+          ],
         ),
       ),
     );
   }
 }
 
+class BabyStat {
+  final String label;
+  final int value;
+
+  BabyStat({required this.label, required this.value});
+}
